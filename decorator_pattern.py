c7e758f86efd214a8ec6aee32a09afe2b16553c9
Unknown <filo.vlada@gmail.com>
2017-05-12 23:12:24 +0200

    minor fixing

--- decorator_pattern.py
+++ decorator_pattern.py
@@ -3,8 +3,8 @@ def markup_text(text, *responsibilities):
     temp_text = text
 
     for tag in responsibilities:
-        if tag == 'b': temp_text = '%s%s%s' % ('<b>', temp_text, '</b>')
-        if tag == 'i': temp_text = '%s%s%s' % ('<i>', temp_text, '</i>')
+        if tag == 'b': temp_text = '%s%s%s' % ('<b>', temp_text, '</b>')#; print ("Bold wrapping executed on " + text)
+        if tag == 'i': temp_text = '%s%s%s' % ('<i>', temp_text, '</i>')#; print ("Italic wrapping executed on " + text)
 
     print (temp_text)
 
@@ -12,7 +12,10 @@ def markup_text(text, *responsibilities):
 ## around some default object (string in this case)
 
 markup_text("Text without markup.")
+markup_text("Wrap me around stuff.", *['b', 'i', 'b'])
+markup_text("Just italic, thank you.", *['i'])
 
-markup_text("Wrap me around stuff.", ['b', 'i', 'b'])
-
-markup_text("Just italic, thank you.", ['i']) 
+## OUTPUT
+# Text without markup.
+# <b><i><b>Wrap me around stuff.</b></i></b>
+# <i>Just italic, thank you.</i>
